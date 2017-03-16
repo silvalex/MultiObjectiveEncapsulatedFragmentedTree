b9017517d4293f0e0c206c91cc07e2503fb79975
Alexandre Sawczuk da Silva <sawczualex@Alexandres-MacBook-Pro.local>
2017-03-16 19:33:11 +1300

    Fixed normalisation bounds and mutation operator.

--- src/wsc/WSCInitializer.java
+++ src/wsc/WSCInitializer.java
@@ -140,8 +140,8 @@ public class WSCInitializer extends SimpleInitializer {
 
 		populateTaxonomyTree();
 		relevant = getRelevantServices(serviceMap, taskInput, taskOutput);
-		//calculateNormalisationBounds(relevant); XXX
-		calculateNormalisationBounds(new HashSet<Service>(serviceMap.values()));
+		calculateNormalisationBounds(relevant); //XXX
+		//calculateNormalisationBounds(new HashSet<Service>(serviceMap.values()));
 		setupTime = System.currentTimeMillis() - startTime;
 	}
 
